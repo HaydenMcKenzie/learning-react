c29363b6149085b32177b770987d2b6a4468ed44
hayden <haydendanielmckenzie@gmail.com>
2024-11-12 03:56:22 +1100

    Added ID

--- src/index.js
+++ src/index.js
@@ -9,29 +9,31 @@ function Greeting() {
 }
 */
 
-function BookList() {
-    return (
-        <section className="booklist">
-            {books.map((book) => {
-                const { img, title, author } = book
-                return <Book img={img} title={title} author={author} />
-            })}
-        </section>
-    )
-}
-
 const books = [
     {
         img: 'https://images-na.ssl-images-amazon.com/images/I/81P9B8sY4AL._AC_UL600_SR600,400_.jpg',
         title: 'How to Catch a Turkey',
         author: 'Adam Wallace',
+        id: 1,
     },
     {
         img: 'https://images-na.ssl-images-amazon.com/images/I/41e+dwz5W5L._AC_UL600_SR600,400_.jpg',
         title: 'Melania',
         author: 'Melania Trump',
+        id: 2,
     },
 ]
+
+function BookList() {
+    return (
+        <section className="booklist">
+            {books.map((book) => {
+                const { img, title, author, id } = book
+                return <Book img={img} title={title} author={author} key={id} />
+            })}
+        </section>
+    )
+}
 
 const Book = (props) => {
     console.log(props)
@@ -44,6 +46,6 @@ const Book = (props) => {
         </article>
     )
 }
-// Or it can be :: const Book = { img, title, author } => {
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<BookList />)
