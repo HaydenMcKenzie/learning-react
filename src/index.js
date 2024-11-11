92057ebe39e2bddfe11c96e1dc3e35dca789988b
hayden <haydendanielmckenzie@gmail.com>
2024-11-11 23:02:20 +1100

    Nested

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+/*
 function Greeting() {
     return <h2>My First Component</h2>
+}
+*/
+
+// Div or Section or React.Fragment
+function Greeting() {
+    return (
+        <div>
+            <Person />
+            <Message />
+        </div>
+    )
+}
+
+const Person = () => <h2>Joe Doe</h2>
+const Message = () => {
+    return <p>This is a paragraph about me</p>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
