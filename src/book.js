acfda09830633f1c191d25c15b05fec9e255e049
root <root@LAPTOP-PPPRAONH.home>
2024-11-14 18:49:47 +1100

    Challenge Completed First Try

--- src/book.js
+++ src/book.js
@@ -1,12 +1,13 @@
 const Book = (props) => {
   //console.log(props);
   const { img, title, author, index } = props;
+  console.log(index);
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      <h4>{index + 1}</h4>
+      <span className="number">{index + 1}</span>
     </article>
   );
 };
