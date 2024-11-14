a04038fa3f82f18679a24d69b94f8c33130dacb4
root <root@LAPTOP-PPPRAONH.home>
2024-11-14 18:03:13 +1100

    Fixing Finding ID via Button

--- src/index.js
+++ src/index.js
@@ -25,28 +25,31 @@ const books = [
 ];
 
 function BookList() {
-  const someValue = 'shakeAndBake';
-  const displayValue = () => {
-    console.log(someValue);
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
   };
+
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} displayValue={displayValue} />;
+        return <Book {...book} key={book.id} getBook={getBook} />;
       })}
     </section>
   );
 }
 
 const Book = (props) => {
-  //console.log(props); passing values from top to bottom
-  const { img, title, author, displayValue } = props;
-
+  //console.log(props);
+  const { img, title, author, getBook, id } = props;
+  const getSingleBook = () => {
+    getBook(id);
+  };
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
-      <button onClick={displayValue}>Display</button>
+      <button onClick={getSingleBook}>Display</button>
       <h4>{author}</h4>
     </article>
   );
