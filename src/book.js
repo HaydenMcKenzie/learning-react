acca7ab79c37c2904094d26eb2d7f7ccd97d024b
root <root@LAPTOP-PPPRAONH.home>
2024-11-14 20:52:00 +1100

    Added Number in Corner w/ Color

--- src/book.js
+++ src/book.js
@@ -7,7 +7,7 @@ const Book = (props) => {
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      <span className="number">{index + 1}</span>
+      <span className="number">{`#${index + 1}`}</span>
     </article>
   );
 };
